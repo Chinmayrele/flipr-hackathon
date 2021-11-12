ad75c95305f172a92e029505c51035b42353ce3a
Sumit Tiware <tiwaresumit143@gmail.com>
2021-11-12 16:42:47 +0530

    initial

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SelectPage extends StatelessWidget {
+  const SelectPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
